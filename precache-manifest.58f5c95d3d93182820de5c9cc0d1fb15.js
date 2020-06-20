7bbced9b18211edf7b8ca93a14844b55432287fd
shubham.d <dogra.shubham466@gmail.com>
2020-06-20 13:30:20 +0530

    Updates

---
+++ precache-manifest.58f5c95d3d93182820de5c9cc0d1fb15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a2ab9cd90d9b5da3c7b431b1bcebde",
+    "url": "/github-pages-react-todo/index.html"
+  },
+  {
+    "revision": "4de744016040a03f865d",
+    "url": "/github-pages-react-todo/static/css/main.e50a69e6.chunk.css"
+  },
+  {
+    "revision": "f6ca22af6a65124561f6",
+    "url": "/github-pages-react-todo/static/js/2.7547e431.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-pages-react-todo/static/js/2.7547e431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de744016040a03f865d",
+    "url": "/github-pages-react-todo/static/js/main.59231c1e.chunk.js"
+  },
+  {
+    "revision": "fdc90064d7d4128e7fb8",
+    "url": "/github-pages-react-todo/static/js/runtime-main.5ff759cc.js"
+  }
+]);
